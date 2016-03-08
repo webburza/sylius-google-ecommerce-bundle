dcc2765d9d5304594847214da86c93021bef7bc4
Dalibor Karlović <dkarlovi@burza.hr>
2016-03-08 13:58:10 +0100

    tests(bundle) initial setup

---
+++ Tests/ClientTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Webburza\Sylius\GoogleEcommerceBundle\Tests;
+
+use PHPUnit_Framework_TestCase as PHPUnitTestCase;
+
+/**
+ * Class ClientTest.
+ */
+class ClientTest extends PHPUnitTestCase
+{
+
+}
